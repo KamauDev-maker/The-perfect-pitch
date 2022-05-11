0e8f7f7404633c604d7b2d4ea4aca2de6329a251
KamauDev-maker <oscarnjenga@gmail.com>
2022-05-11 15:30:07 +0300

    creating a category clas s model

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ from . import db
 
 
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     
     __tablename__ ='users'
     id = db.Column(db.Integer,primary_key = True)
@@ -45,6 +45,27 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+    
+    
+class PitchCategory(db.Model):
+    
+    __tablename__ = 'categories'
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+
+
+    def save_category(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_categories(cls):
+        categories = PitchCategory.query.all()
+        return categories
+  
 
 
 
