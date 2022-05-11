ab3415b735d35bb81152635e1b243091b08e3040
KamauDev-maker <oscarnjenga@gmail.com>
2022-05-11 15:09:58 +0300

    setting up SQLAlchemy

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://oscar:1803@localhost/oneminpitch'
 
 
 class ProdConfig(Config):
