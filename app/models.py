22ccd8a15b275f790d0b3747c3834ad3ed0e176f
KamauDev-maker <oscarnjenga@gmail.com>
2022-05-11 15:27:33 +0300

    securing password

---
+++ app/models.py
@@ -0,0 +1,53 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
+from . import login_manager
+from datetime import datetime
+from sqlalchemy.sql import func
+from . import db
+
+
+
+
+
+class User(db.Model):
+    
+    __tablename__ ='users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True, index =True)
+    password_hash = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pitches = db.relationship("Pitch", backref="user", lazy = "dynamic")
+    comment = db.relationship("Comments", backref="user", lazy = "dynamic")
+    vote = db.relationship("Votes", backref="user", lazy = "dynamic")
+    
+    
+    @property
+    def password(self):
+        raise AttributeError('You can not read the password Attribute')
+
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+
+
+
+
