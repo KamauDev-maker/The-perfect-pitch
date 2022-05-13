282a15b1134836a280be2d5a19631dddd0ee9d7b
KamauDev-maker <oscarnjenga@gmail.com>
2022-05-13 14:29:16 +0300

    deploye to github

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://oscar:1803@localhost/oneminpitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://oscar:1803@localhost/oneminpitch'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
